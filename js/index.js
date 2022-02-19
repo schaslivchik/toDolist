8f577bd2da2f4460ed0e610156c7969b515cc5fc
Mikhail <schaslivchikm@gmail.com>
2022-02-19 18:35:12 +0200

    initial

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const newToDoInput = document.querySelector(".new-todo");
+
+const addBtn = document.querySelector("button");
+const toDoListItem = document.querySelector(".todos-list");
+
+addBtn.onclick = (e) => {
+  const REG_EXP = /^\s*$/;
+  if (!REG_EXP.test(newToDoInput.value)) {
+    newToDoInput.classList.remove("invalid");
+
+    const newListItem = document.createElement("li");
+    newListItem.classList.add("list-item");
+
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "-";
+
+    toDoListItem.append(newListItem);
+    newListItem.append(newToDoInput.value, removeBtn);
+    newToDoInput.value = "";
+
+    removeBtn.onclick = (e) => {
+      e.currentTarget.closest(".list-item").remove();
+    };
+  } else {
+    newToDoInput.classList.add("invalid");
+  }
+};
